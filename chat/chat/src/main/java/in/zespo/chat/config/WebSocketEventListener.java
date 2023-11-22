52d32e7c7d2eef812890ee20635813311cef4942
abhayjohri23 <abhay.johri0@gmail.com>
2023-11-22 19:54:05 +0530

    Added Backend and front end. JS is pending

---
+++ chat/chat/src/main/java/in/zespo/chat/config/WebSocketEventListener.java
@@ -0,0 +1,2 @@
+package in.zespo.chat.config;public class WebSocketEventListener {
+}
---
+++ chat/chat/src/main/resources/static/script.js
@@ -0,0 +1,16 @@
+const colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+const userNameForm = document.querySelector('#userNameForm');
+const chatForm = document.querySelector('#chatForm');
+const connectingStatus = document.querySelector('#connectingStatus');
+const messageArea = document.querySelector('#messageArea');
+const messageInput = document.querySelector('#messageInput');
+const userInputField = document.querySelector('.inputField');
+
+const enterButton = document.querySelector('#enterButton');
+const sendBtn = document.querySelector('#sendBtn');
+
+
